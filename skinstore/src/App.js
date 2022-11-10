41bbeeb5b99d74defe8edbe0a8047a8a24a7266e
Deepak Mane <thedpmane@gmail.com>
2022-11-11 00:05:03 +0530

    day3 some part

--- skinstore/src/App.js
+++ skinstore/src/App.js
@@ -1,14 +1,15 @@
 import "./styles.css";
-import Navbar from "./components/Navbar";
 
 import { ChakraProvider } from "@chakra-ui/react";
+// import Home from "./components/Home";
+import CaptionCarousel from "./components/Home";
 //import SidebarWithHeader from "./components/navbar";
 
 export default function App() {
   return (
     <div className="App">
       <ChakraProvider>
-        <Navbar position="fixed" />
+      <CaptionCarousel/>
       </ChakraProvider>
     </div>
   );
